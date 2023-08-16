81710e9ffaae2fc2f6550a038771c925500e7080
Nikhil <NikhilC2209@gmail.com>
2023-08-17 01:11:18 +0530

    Added sample test mixtures

--- musdb_data.py
+++ musdb_data.py
@@ -1,14 +1,18 @@
+#!pip install musdb
+
 import musdb
 from config import *
 
 mus = musdb.DB(subsets='test', root=WAV_PATH)
 
-#!musdbconvert WAV_PATH
+#! musdbconvert path/to/musdb-stems-root path/to/new/musdb-wav-root
+
+## Convert from STEMS format to .wav format
 
 for tracks in mus:
     print(tracks)
 
 print(type(mus[0]))
 
-mus[0].audio
+print(mus[0].audio)
 
